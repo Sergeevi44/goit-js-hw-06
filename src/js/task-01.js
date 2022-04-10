d905553255ae0ce4dc9252c83f24d02eeca859c4
Oleksii <Sergeevi44@gmail.com>
2022-04-10 05:23:39 +0300

    Subtask 1 done

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,10 +1,16 @@
-/* 
-!Напиши скрипт, який: 
-*Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
-*Для кожного элемента li.item у спику ul#categories, знайде і виведе в консоль текст заголовку елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).*/
+/*
+ *Напиши скрипт, який:
+ *Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+ *Для кожного элемента li.item у спику ul#categories, знайде і виведе в консоль текст заголовку елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).
+ */
 
 const refs = {
 	categories: document.querySelectorAll("#categories .item"),
 	header: document.querySelector(".item h2"),
 	elements: document.querySelectorAll(".item li"),
 };
+
+//TODO: Subtask 1:
+
+const quantityOfCategories = refs.categories.length;
+console.log(`Number of categories: ${quantityOfCategories}`);
