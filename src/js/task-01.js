73cb633ad50f9fa2a090ae9839dc6171e256f854
Oleksii <Sergeevi44@gmail.com>
2022-04-10 05:13:49 +0300

    Create refs

---
+++ src/js/task-01.js
@@ -0,0 +1,10 @@
+/* 
+!Напиши скрипт, який: 
+*Порахує і виведе в консоль кількість категорій в ul#categories, тобто елементів li.item.
+*Для кожного элемента li.item у спику ul#categories, знайде і виведе в консоль текст заголовку елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).*/
+
+const refs = {
+	categories: document.querySelectorAll("#categories .item"),
+	header: document.querySelector(".item h2"),
+	elements: document.querySelectorAll(".item li"),
+};
